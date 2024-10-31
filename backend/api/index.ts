e37c51dface45e2769238c14a30269f0bbc05ac1
Víctor Reyes <unhill@gmail.com>
2024-10-31 13:08:16 +0100

    Fix vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
-export const app = express();
+const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on vercel");
+  res.send("Express on Vercel");
 });
 
-app.listen(3000, () => {
-  console.log("Server is ready on port 3000");
-});
+// app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
