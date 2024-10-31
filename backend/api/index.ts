18ead61815b7af4e6cfe12bfd471aa2d9b82392d
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-31 14:49:31 +0100

    added post endpoint that works locally but not working on vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,15 +1,23 @@
 import express from "express";
 
+const str: string = "hello";
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-
 app.get("/api/v1/mobs", (req, res) => {
-  res.send("all mobs")
-})
+  res.send("all mobs");
+});
+
+app.post("/api/v1/mobs", (req, res) => {
+  console.log(req.body);
+  res.send("Hajsan");
+  return;
+});
 
 export default app;
-
